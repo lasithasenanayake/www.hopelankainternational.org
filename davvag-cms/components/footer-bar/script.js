741432181b96d30f56761bc11c1117d3b2a700b9
Lasitha Senanayake <lasitha.senanayake@gmail.com>
2019-08-14 18:00:26 +0530

    Revert "add all"
    This reverts commit 1822c128b1349a21aec19fb97cb0fa9664591898.

--- davvag-cms/components/footer-bar/script.js
+++ davvag-cms/components/footer-bar/script.js
@@ -1,9 +1,26 @@
 WEBDOCK.component().register(function(exports){
-    var bindData ={
-        SocailButtons:[]
-    };
+   
     var vueData = {
-        data:bindData,
+        data:{
+            userData: {
+                email : "Loading",
+                
+
+            },facebook:"https://www.facebook.com/hopecenter.north",
+            youtube:"https://www.youtube.com/channel/UCmr-voS45Jzn4yUE8wqCuzA",
+            gpluse:undefined,
+            twitter:undefined,
+            instergram:undefined,
+            contact:"+94777133777",
+            appData : {
+                title:""
+            },
+            appKey : "",
+            name:"Davvag",
+            icon:"http://citrusengine.com/wp-content/themes/citrus/images/logo/citrus-logo-2D.png",
+            TopButtons:[{id:"home",caption:"Home",url:"#app",class:"nav-item",sub:[]},
+                       {id:"about",caption:"About",url:"#about",class:"nav-item dropdown",sub:[{id:"home",caption:"Home",url:"#app",sub:[]},{id:"home",caption:"Home",url:"#app",sub:[]}]}]
+        },
         methods: {
             signout: signout,
             submitSearch: function(event){
@@ -26,31 +43,40 @@ WEBDOCK.component().register(function(exports){
 
     exports.onReady = function(element){
         vueData.el = '#' + $(element).attr('id');
+        var handler  = exports.getComponent("auth-handler");
+        document.title="";
+        console.log(getCookie("securityToken"));
+        handler.services.Session(getCookie("securityToken"))
+                .then(function(result){
+                    if(result.result!=null){
+                        vueData.data.userData=result.result;
+                        console.log(result);
+                    }else{
+                       // signout();
+                    }
+                })
+                .error(function(){
+                    ///signout();
+                });
         
-        //document.title="";
-        //console.log(getCookie("securityToken"));
         new Vue(vueData);
-        var menuhandler  = exports.getComponent("soss-data");
-        var query=[{storename:"d_cms_buttons_v1",search:"BType:social-footer"}];
-        var tmpmenu=[];
-        bindData.TopButtons=[];
-        menuhandler.services.q(query)
-                    .then(function(r){
-                        console.log(JSON.stringify(r));
-                        if(r.success){
-                            //bindData.TopButtons=r.result.d_cms_buttons_v1;
-                            //tmpmenu=r.result.d_cms_buttons_v1;
-                            bindData.SocailButtons=r.result.d_cms_buttons_v1;
-                        }
-                    })
-                    .error(function(error){
-                        console.log(error.responseJSON);
-        });
-        
-       
     }
 
-    
+    function getCookie(cname) {
+        var name = cname + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for(var i = 0; i <ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
 
     exports.setDisplayData = function(appKey,appData) {
         vueData.data.appData = appData;
--- davvag-cms/pages/index.php
+++ davvag-cms/pages/index.php
@@ -1,17 +1,3 @@
-<?php
-    require_once(PLUGIN_PATH . "/phpcache/cache.php");
-    require_once(PLUGIN_PATH . "/auth/auth.php");
-    $domain = Auth::GetDomainAttributes();
-    
-    if(isset($domain->name)){
-        define("DOMAIN",$domain->domain);
-        define("DOMAINNAME",$domain->name);
-
-    }else{
-        define("DOMAIN","NotReg-405");
-        define("DOMAINNAME","This Domain is not registered please contact davvag.com for information");
-    }
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,14 +5,14 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
   <meta name="description" content="">
   <meta name="author" content="">
-  <link rel="icon" href="assets/dock/images/favicon.ico" type="image/png">
-  <title><?php echo DOMAINNAME; ?></title>
-  <link href="assets/davvag-cms-davvag/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/davvag-cms-davvag/css/dockanimation.css" rel="stylesheet">
-  <link href="assets/davvag-cms-davvag/css/custom_v1.css" rel="stylesheet">
-  <link href="assets/davvag-cms-davvag/css/style.loaders.css" rel="stylesheet">
+  <link rel="icon" href="assets/davvag-cms/images/favicon.png" type="image/png">
+  <title>Hope Lanka International</title>
+  <link href="assets/davvag-cms/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/davvag-cms/css/dockanimation.css" rel="stylesheet">
+  <link href="assets/davvag-cms/css/custom.css" rel="stylesheet">
+  <link href="assets/davvag-cms/css/style.loaders.css" rel="stylesheet">
   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha256-3dkvEK0WLHRJ7/Csr0BZjAWxERc5WH7bdeUya2aXxdU= sha512-+L4yy6FRcDGbXJ9mPG8MT/3UCDzwR9gPeyFNMCtInsol++5m3bk2bXWKdZjvybmohrAsn3Ua5x8gfLnbE1YkOg==" crossorigin="anonymous">
-  <script id="facebook-jssdk" src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&amp;version=v3.1&amp;appId=2048153755495242&amp;autoLogAppEvents=1"></script>
+ 
 </head>
 
 <body>
@@ -44,13 +30,11 @@
 
 <div  webdock-component="footer-bar" id="id-footer-bar">
     </div>
-
-
-<script src="assets/davvag-cms-davvag/js/bootstrap.min.js"></script>
-<script src="assets/davvag-cms-davvag/js/moments.js"></script>
-<script src="assets/davvag-cms-davvag/js/masory.min.js"></script>
+<script src="assets/davvag-cms/js/bootstrap.min.js"></script>
+<script src="assets/davvag-cms/js/moments.js"></script>
+<script src="assets/davvag-cms/js/masory.min.js"></script>
 <script src="lib/jquery.js"></script>
-<script src="lib/webdock.js" webdockapp="davvag-cms-davvag"></script>
+<script src="lib/webdock.js" webdockapp="davvag-cms"></script>
 
 
 <script type="text/javascript">
